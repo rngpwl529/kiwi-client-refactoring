8a0bc4d81f009cffdd043a8e1fff13207f4186d3
hyungjoo kim <kim62210@gmail.com>
2021-07-06 15:37:19 +0900

    mainpage/forceGraph

--- src/pages/MainContainer.jsx
+++ src/pages/MainContainer.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import { React, useCallback } from 'react';
 import MainSearchBar from "../components/Main/MainSearchBar"
 import MapController from "../components/Main/MapController"
 import NodeOption from "../components/Main/NodeOption"
@@ -11,13 +11,28 @@ import UserSetting from "../components/Main/UserSetting"
 import NodeMap from '../components/Main/NodeMap'
 import Header from '../containers/Header'
 
+import ForceGraph from '../components/Main/forceGraph/forceGraph';
+import data from '../data/data.json';
 const MainContainer = () => {
   console.log(`${MainSearchBar},${MapController},${NodeOption},${NodeSetting},${SignIn},${SignUp},${SiteSetting},${UserSetting}`);
+  const nodeHoverTooltip = useCallback((node, x, y) => {
+    return `<div> ${x}, ${y}
+  <b> id : ${node.id}</b>
+  <b> name : ${node.name}</b>
+  <b> gender : ${node.gender}</b>
+</div>`;
+}, []);
   return(
     <div id='main-container'>
       <NodeMap />
       <Header />
-      <SiteSetting/>
+      <section className="Main">
+                <ForceGraph
+                    linksData={data.links}
+                    nodesData={data.nodes}
+                    nodeHoverTooltip={nodeHoverTooltip}
+                />
+            </section>
     </div>
   )
 }
