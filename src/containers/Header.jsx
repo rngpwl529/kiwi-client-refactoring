ca74272fdb9c3d54d1a203fbc03c30a8872bfaaa
rngpwl529 <rngpdnjs12@gmail.com>
2021-07-06 11:07:38 +0900

    키워드/목업 및 기능

--- src/containers/Header.jsx
+++ src/containers/Header.jsx
@@ -1,7 +1,19 @@
 import MapController from '../components/Main/MapController'
 import MainSearchBar from '../components/Main/MainSearchBar'
-import React from 'react';
+import React,{useState} from 'react';
+import Keyword from '../components/Main/Keyword'
+
 const Header = ()=>{
+  const [keywordOpen, setKeywordOpen] = useState(false)
+  const keywordHandler=()=>{
+    if(!keywordOpen){
+      setKeywordOpen(true)
+    }
+    else{
+      setKeywordOpen(false)
+    }
+  }
+
   return(
     <div id = 'header'>
       <img src='images/bitmap.png' alt='KiWi logo' className='logo'/>
@@ -10,9 +22,10 @@ const Header = ()=>{
         <MainSearchBar />
         <ion-icon name="person-outline"></ion-icon>
         <ion-icon name="settings-outline"></ion-icon>
-        <ion-icon name="bookmark-outline"></ion-icon>
+        <ion-icon name="bookmark-outline" onClick={keywordHandler}></ion-icon>
         <ion-icon name="image-outline"></ion-icon>
       </div>
+      {keywordOpen ? <Keyword setKeywordOpen={setKeywordOpen}/>:null}
     </div>
   )
 }
