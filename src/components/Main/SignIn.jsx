3a7abc3b46cadb5caeb76eb0824be7bf5cff2f4b
rngpwl529 <rngpdnjs12@gmail.com>
2021-07-07 17:56:26 +0900

    로그인/ui

--- src/components/Main/SignIn.jsx
+++ src/components/Main/SignIn.jsx
@@ -1,18 +1,38 @@
 import axios from 'axios';
-import React,{ useState } from 'react';
+import React,{ useEffect, useState } from 'react';
 import { useDispatch } from 'react-redux';
-import { closeSigninModal, openSignupModal } from '../../redux/modalstatus';
+import { closeSigninModal } from '../../redux/modalstatus';
+import { emailValid, passwordValid } from '../../utils/validCheck'
+import SignUp from './SignUp'
 
 const SignIn = () => {
-
     let SERVER_URL = process.env.REACT_APP_SERVER_URL
 
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
-
+    const [signup, setSignup] = useState('')
     const dispatch = useDispatch();
+    let emailInput = document.querySelector('.emailInput')
+    let passwordInput = document.querySelector('.passwordInput')
+    //이메일, 비밀번호 유효성 검사
+    useEffect(()=>{
+        if(emailValid(email)){
+            emailInput.classList.remove('invalid')
+        }
+        else{
+            emailInput.classList.add('invalid')
+        }
+        if(passwordValid(password)){
+            passwordInput.classList.remove('invalid')
+        }
+        else{
+            passwordInput.classList.add('invalid')
+        }
 
-    const handleOnClick = ()=>{
+    },[email,password])
+
+
+    const handleSignIn = ()=>{
         axios
         .post(
             `${SERVER_URL}/users/signin`,
@@ -24,6 +44,7 @@ const SignIn = () => {
         .then(res=>{
             console.log(res)
             console.log('로그인 성공!')
+            dispatch(closeSigninModal())
         })
         .catch(err=>{
             console.log(err)
@@ -32,60 +53,66 @@ const SignIn = () => {
     const handleCloseButtonClick = () => {
         dispatch(closeSigninModal());
     }
-    const handleSignupClick = () => {
-        dispatch(closeSigninModal());
-        dispatch(openSignupModal());
+
+    const handleSignup=()=>{
+        setSignup(true)
     }
+
 
     return (
         <div className="darkbackground">
             <div id='signin-container'>
                 <div className='sign-img'></div>
-                <div id='signin-form'>
-                    <ion-icon name="close-outline" onClick={handleCloseButtonClick}></ion-icon>
-                    <span className='title'>SignIn</span>
-                    <div className='box'>
-                        <div className='input'>
-                            <ion-icon name='person-outline'></ion-icon>
-                            <input
-                                type='text'
-                                placeholder='email'
-                                onChange={event => {
-                                    setEmail(event.target.value);
-                                }}
-                            ></input>
-                            <ion-icon name='checkmark-circle-outline'></ion-icon>
+                {!signup ?
+                    <div id='signin-form'>
+                        <ion-icon name="close-outline" onClick={handleCloseButtonClick}></ion-icon>
+                        <span className='title'>SignIn</span>
+                        <div className='box'>
+                            <div className='input emailInput'>
+                                <ion-icon name='person-outline'></ion-icon>
+                                <input
+                                    type='text'
+                                    placeholder='email'
+                                    className='email'
+                                    onChange={event => {
+                                        setEmail(event.target.value);
+                                    }}
+                                ></input>
+                                <ion-icon name='checkmark-circle-outline'></ion-icon>
+                            </div>
+                            <div className='input passwordInput'>
+                                <ion-icon name='lock-closed-outline'></ion-icon>
+                                <input
+                                    type='password'
+                                    placeholder='password'
+                                    className='password'
+                                    onChange={event => {
+                                        setPassword(event.target.value);
+                                    }}
+                                ></input>
+                                <ion-icon name='close-circle-outline'></ion-icon>
+                            </div>
                         </div>
-                        <div className='input'>
-                            <ion-icon name='lock-closed-outline'></ion-icon>
-                            <input
-                                type='password'
-                                placeholder='password'
-                                onChange={event => {
-                                    setPassword(event.target.value);
-                                }}
-                            ></input>
-                            <ion-icon name='close-circle-outline'></ion-icon>
+                        <div className='submit'>
+                            <div
+                                className='btn sign'
+                                onClick={handleSignIn}
+                            >
+                                로그인
+                            </div>
+                            <div className='splitter'>
+                                <div className='line'></div>
+                                <div>OR</div>
+                                <div className='line'></div>
+                            </div>
+                            <div className='btn kakao'>카카오 로그인</div>
                         </div>
+                        <span className='link'>
+                            아직 회원이 아니신가요? <span onClick={handleSignup}>회원가입하기</span>
+                        </span>
                     </div>
-                    <div className='submit'>
-                        <div
-                            className='btn sign'
-                            onClick={handleOnClick}
-                        >
-                            로그인
-                        </div>
-                        <div className='splitter'>
-                            <div className='line'></div>
-                            <div>OR</div>
-                            <div className='line'></div>
-                        </div>
-                        <div className='btn kakao'>카카오 로그인</div>
-                    </div>
-                    <span className='link'>
-                        아직 회원이 아니신가요? <span onClick={handleSignupClick}>회원가입하기</span>
-                    </span>
-                </div>
+                    :<SignUp setSignup={setSignup} handleCloseButtonClick={handleCloseButtonClick}/>
+                }
             </div>
         </div>
     );
--- src/components/Main/SignUp.jsx
+++ src/components/Main/SignUp.jsx
@@ -1,19 +1,16 @@
 import axios from 'axios';
 import React,{ useState } from 'react';
-import { useDispatch } from 'react-redux';
-import { closeSignupModal, openSigninModal } from '../../redux/modalstatus';
 
-const SignUp = () => {
+const SignUp = ({setSignup, handleCloseButtonClick}) => {
 
     let SERVER_URL = process.env.REACT_APP_SERVER_URL
 
     const [email, setEmail] = useState("");
-    const [username, setUsername] = useState("");
+    const [userName, setUsername] = useState("");
     const [password, setPassword] = useState("");
     const [passwordCheck, setPasswordCheck] = useState("");
     console.log(passwordCheck)
 
-    const dispatch = useDispatch();
 
     const handleOnClick= ()=>{
         axios
@@ -21,90 +18,94 @@ const SignUp = () => {
             `${SERVER_URL}/users/signup`,
             {
                 email,
-                username,
+                userName,
                 password,
             }
         )
+        .then((res)=>{
+                console.log(res)
+                console.log('회원가입 성공!!')
+                setSignup(false)
+            }
+        )
+        .catch((err)=>{
+                console.log(err.response)
+            }
+        )
     }
-    const handleCloseButtonClick = () => {
-        dispatch(closeSignupModal());
-    }
-    const handleSigninClick = () => {
-        dispatch(closeSignupModal());
-        dispatch(openSigninModal());
+
+    const handleSignIn = ()=>{
+        setSignup(false)
     }
 
     return (
-        <div className="darkbackground">
-            <div id='signin-container'>
-                <div className='sign-img'></div>
-                <div id='signin-form'>
-                <ion-icon name="close-outline" onClick={handleCloseButtonClick}></ion-icon>
-                    <span className='title'>SignUp</span>
-                    <div className='box'>
-                        <div className='input'>
-                            <ion-icon name='person-outline'></ion-icon>
-                            <input type='text'
-                                placeholder='email'
-                                onChange={event => {
-                                    setEmail(event.target.value);
-                                }}
-                            ></input>
-                            <ion-icon name='checkmark-circle-outline'></ion-icon>
-                        </div>
-                        <div className='input'>
-                            <ion-icon name='person-outline'></ion-icon>
-                            <input
-                                type='text'
-                                placeholder='username'
-                                onChange={event => {
-                                    setUsername(event.target.value);
-                                }}
-                            ></input>
-                            <ion-icon name='checkmark-circle-outline'></ion-icon>
-                        </div>
-                        <div className='input'>
-                            <ion-icon name='lock-closed-outline'></ion-icon>
-                            <input
-                                type='password'
-                                placeholder='password'
-                                onChange={event => {
-                                    setPassword(event.target.value);
-                                }}
-                            ></input>
-                            <ion-icon name='close-circle-outline'></ion-icon>
-                        </div>
-                        <div className='input'>
-                            <ion-icon name='lock-closed-outline'></ion-icon>
-                            <input
-                                type='password'
-                                placeholder='password check'
-                                onChange={event => {
-                                    setPasswordCheck(event.target.value);
-                                }}
-                            ></input>
-                            <ion-icon name='close-circle-outline'></ion-icon>
-                        </div>
-                    </div>
-                    <div className='submit'>
-                        <div
-                            className='btn sign'
-                            onClick={handleOnClick}
-                        >
-                            로그인
-                        </div>
-                        <div className='splitter'>
-                            <div className='line'></div>
-                            <div>OR</div>
-                            <div className='line'></div>
-                        </div>
-                        <div className='btn kakao'>카카오 로그인</div>
-                    </div>
-                    <span className='link'>
-                        이미 회원이신가요? <span onClick={handleSigninClick}>로그인하기</span>
-                    </span>
+        <div id='signin-form'>
+            <ion-icon name="close-outline" onClick={handleCloseButtonClick}></ion-icon>
+            <span className='title'>SignUp</span>
+            <div className='box'>
+                <div className='input'>
+                    <ion-icon name='person-outline'></ion-icon>
+                    <input type='text'
+                        placeholder='email'
+                        onChange={event => {
+                            setEmail(event.target.value);
+                        }
+                        
+                    }
+                    ></input>
+                    <ion-icon name='checkmark-circle-outline'></ion-icon>
+                </div>
+                <div className='input'>
+                    <ion-icon name='person-outline'></ion-icon>
+                    <input
+                        type='text'
+                        placeholder='username'
+                        onChange={event => {
+                            setUsername(event.target.value);
+                        }}
+                    ></input>
+                    <ion-icon name='checkmark-circle-outline'></ion-icon>
+                </div>
+                <div className='input'>
+                    <ion-icon name='lock-closed-outline'></ion-icon>
+                    <input
+                        type='password'
+                        placeholder='password'
+                        onChange={event => {
+                            setPassword(event.target.value);
+                        }}
+                    ></input>
+                    <ion-icon name='close-circle-outline'></ion-icon>
+                </div>
+                <div className='input'>
+                    <ion-icon name='lock-closed-outline'></ion-icon>
+                    <input
+                        type='password'
+                        placeholder='password check'
+                        onChange={event => {
+                            setPasswordCheck(event.target.value);
+                        }}
+                    ></input>
+                    <ion-icon name='close-circle-outline'></ion-icon>
                 </div>
             </div>
+            <div className='submit'>
+                <div
+                    className='btn sign'
+                    onClick={handleOnClick}
+                >
+                    회원가입
+                </div>
+                <div className='splitter'>
+                    <div className='line'></div>
+                    <div>OR</div>
+                    <div className='line'></div>
+                </div>
+                <div className='btn kakao'>카카오 회원가입</div>
+            </div>
+            <span className='link'>
+                이미 회원이신가요? <span onClick={handleSignIn}>로그인하기</span>
+            </span>
         </div>
     );
 };
