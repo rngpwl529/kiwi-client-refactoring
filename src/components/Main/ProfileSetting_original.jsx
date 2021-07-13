c1cd13f9e4695bdbae0b50e4e196d7781d159dd1
jaehwan <jh968574@gmail.com>
2021-07-13 13:05:15 +0900

    capture기능 추가

---
+++ src/components/Main/ProfileSetting_original.jsx
@@ -0,0 +1,50 @@
+import { useState, React } from "react";
+import { useDispatch, useSelector } from 'react-redux';
+import { openUserinfoModal, openSigninModal, openSettingModal } from '../../redux/modalstatus';
+import Keyword from "../Main/Keyword";
+
+
+const ProfileSetting = ()=>{
+
+    const [keywordOpen, setKeywordOpen] = useState(false)
+    const dispatch = useDispatch();
+    const store = useSelector((state)=>(state))
+
+    const keywordHandler=()=>{
+        if(!keywordOpen /*&& store.sign.isSignIn*/){
+            setKeywordOpen(true)
+        }
+        else if(!store.sign.isSignIn){
+            dispatch(openSigninModal())
+        }
+        else{
+            setKeywordOpen(false)
+        }
+    }
+    const handleProfileClick = () => {
+        if (store.sign.isSignIn) {
+            dispatch(openUserinfoModal());
+        } else {
+            dispatch(openSigninModal());
+        }
+    }
+    const handleSettingClick = () => {
+        if(store.sign.isSignIn){
+            dispatch(openSettingModal());
+        }
+        else{
+            dispatch(openSettingModal())
+        }
+    }
+    return (
+        <div id='profile-container'>
+            <ion-icon name="person-outline" onClick={() => { handleProfileClick(); }} ></ion-icon>
+            <ion-icon name="settings-outline" onClick={() => { handleSettingClick(); }}></ion-icon>
+            <ion-icon name="bookmark-outline" onClick={() => { keywordHandler(); }}></ion-icon>
+            <ion-icon name="image-outline"></ion-icon>
+            {keywordOpen ? <Keyword setKeywordOpen={setKeywordOpen}/>:null}
+        </div>
+    )
+}
+
+export default ProfileSetting
